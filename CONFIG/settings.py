f45038faf8ff73008c22ebddc46d548bc209836e
Aybek <aybek.djumashev@gmail.com>
2025-08-14 14:24:01 +0500

    .

--- CONFIG/settings.py
+++ CONFIG/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 import os
 STATIC_URL = 'static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Default primary key field type
