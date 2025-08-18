a121e7bb39ffbe6444ecff2c60d31031526d8fb1
Aybek <aybek.djumashev@gmail.com>
2025-08-18 22:48:30 +0500

    .

--- transcriber/crons.py
+++ transcriber/crons.py
@@ -1,21 +1,26 @@
 from .models import AudioTranscription
 from django.db.models import Q
 from google import genai
+import time
 
-client = genai.Client(api_key='AIzaSyB0T2_THrBA1mf3pZVwHfGQcRWQ7k0bDn4')
+keys = ['AIzaSyB0T2_THrBA1mf3pZVwHfGQcRWQ7k0bDn4', 'AIzaSyAHhU7dKyfXFUiX78rd4L7mSzubDmk-ZSI']
 
-def generate_text_last_audio():  
-    try:  
-        audio = AudioTranscription.objects.filter(
-            Q(transcription_text__isnull=True) | Q(transcription_text="")
-        ).first()
-        
-        myfile = client.files.upload(file=audio.audio_file.path)
-        response = client.models.generate_content(
-            model="gemini-2.5-pro", contents=["This is a Karakalpak audio. Create a transcription of the speech in the Karakalpak language and the Latin alphabet.", myfile]
-        )
-        audio.transcription_text = response.text
-        audio.save()
+def generate_text_last_audio():
+    for key in keys:  
+        try:  
+            client = genai.Client(api_key=key)
+            audio = AudioTranscription.objects.filter(
+                Q(transcription_text__isnull=True) | Q(transcription_text="")
+            ).first()
+            
+            myfile = client.files.upload(file=audio.audio_file.path)
+            response = client.models.generate_content(
+                model="gemini-2.5-pro", contents=["This is a Karakalpak audio. Create a transcription of the speech in the Karakalpak language and the Latin alphabet.", myfile]
+            )
+            audio.transcription_text = response.text
+            audio.save()
 
-    except Exception as e:  
-        print(e)
+        except Exception as e:  
+            print(e)
+        else:
+            time.sleep(10)
